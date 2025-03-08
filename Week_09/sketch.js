e49d4b48c9e4ad98ceb4fc992c04f1c3246e8482
Kenji Okura <okurakeng@gmail.com>
2025-03-08 09:58:08 -0500

    Update sketch.js

--- Week_09/sketch.js
+++ Week_09/sketch.js
@@ -1,4 +1,4 @@
-let xLocation = 0;
+
 
 function setup() {
   createCanvas(400, 400);
@@ -9,10 +9,5 @@ function draw() {
   background(220);
   fill("orange");
 
-  for (let i = 1; (i*75) <= height; i++) {
-    circle(xLocation, (i*75), 20);
-  }
-
-  if (xLocation < width) xLocation++;
-  else xLocation = 0;
+  
 }
